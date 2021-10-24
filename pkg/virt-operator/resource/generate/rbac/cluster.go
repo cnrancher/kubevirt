175e58d091d4e0a5c1678b74f8c87388c4b16fae
futuretea <Hang.Yu@suse.com>
2021-10-24 20:44:44 +0800

    virt-operator: support soft reboot
    Signed-off-by: futuretea <Hang.Yu@suse.com>

--- pkg/virt-operator/resource/generate/rbac/cluster.go
+++ pkg/virt-operator/resource/generate/rbac/cluster.go
@@ -143,6 +143,7 @@ func newAdminClusterRole() *rbacv1.ClusterRole {
 					"virtualmachineinstances/removevolume",
 					"virtualmachineinstances/freeze",
 					"virtualmachineinstances/unfreeze",
+					"virtualmachineinstances/softreboot",
 				},
 				Verbs: []string{
 					"update",
@@ -233,6 +234,7 @@ func newEditClusterRole() *rbacv1.ClusterRole {
 					"virtualmachineinstances/removevolume",
 					"virtualmachineinstances/freeze",
 					"virtualmachineinstances/unfreeze",
+					"virtualmachineinstances/softreboot",
 				},
 				Verbs: []string{
 					"update",
